f5da30fc92805c38c99b5a0ab22b3707dd6715cf
Matthew Snape <Matt.Snape@warwick.ac.uk>
2022-11-16 23:16:19 +0000

    maps are working

--- src/MPAGSCipher/PlayfairCipher.cpp
+++ src/MPAGSCipher/PlayfairCipher.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <map>
 
 PlayfairCipher::PlayfairCipher(const std::string& key){
 
@@ -31,7 +32,7 @@ void PlayfairCipher::setkey(const std::string& key) {
 
     // Change J -> I (not working)
 
-    /*std::transform(key_.begin(),key.end(), key_.begin(), 
+    std::transform(key_.begin(),key_.end(), key_.begin(), 
         [] (char c) 
         {
             if (c == 'J') return 'I';
@@ -39,28 +40,64 @@ void PlayfairCipher::setkey(const std::string& key) {
         
         }
         
-    );*/
-
-    std::cout << key_ << std::endl;
+    );
 
     std::string dup_char = {""};
 
     //Remove duplicated letters (not working?)
 
     auto dup_det = [&] (char c){
-
-        if (dup_char.find(c) == std::string::npos){
+        if (dup_char.find(c) == std::string::npos)
+        {
+                dup_char += c;
                 return false;
-        } else {
+        } 
+        else 
+        {
                 return true;
         }
 
     };
     
     auto rem_iter2 = std::remove_if(key_.begin(),key_.end(), dup_det);
-    key_.erase(rem_iter2,key_.end());
+    key_.erase(rem_iter2, key_.end());
+
+    std::cout << key_ << std::endl;
+
+    using str2cord_map = std::map<std::string,std::pair<int,int>>;
+
+    using cord2str_map = std::map<std::pair<int,int>,std::string>;
+
+    str2cord_map map1;
+
+    cord2str_map map2;
+
+    int x = 1;
+
+    int y = 1;
+
+    for (size_t i = 0; i < key_.length();i++)
+    {
+        std::pair<int, int > cord{y,x};
+
+        std::pair<std::string,std::pair<int,int>> p0{std::string{key_[i]},cord};
+
+        std::pair<std::pair<int,int>,std::string> p1{cord,std::string{key_[i]}};
+
+        map1.insert( p0 );
+
+        map2.insert( p1 );
+
+        x++;
+
+        switch (x){
+            case 6:
+                x = 1;
+                y++;
+        }
 
 
+    }
 
     //Store the coords of each letter
 
